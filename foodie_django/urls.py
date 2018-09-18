349ef14eb83b4b19e7acdba4972b89f9692c3d01
Alan <a386226@gmail.com>
2018-09-18 16:44:44 -0700

    added templates and views

--- foodie_django/urls.py
+++ foodie_django/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from foodie import views
 
-urlpatterns = [
+urlpatterns=[
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about')
+    # path('special', views.special, name='special'),
+    # path('foodie/', include('foodie.urls')),
+    # path('logout', views.user_logout, name='logout'),
 ]
