49fde24f09ab6b2cd12a69a4cf17bc57f46d0cf5
wongsitu <sa_waika_4d_23@hotmail.com>
2018-09-18 15:46:25 -0700

    Finished coding database

--- foodie/models.py
+++ foodie/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
+
+class Preference(models.Model):
+    api_id = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cuisine
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    preferences = models.ManyToManyField(Preference)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Restaurant(models.Model):
+    user = models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name='restaurant')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    cuisine = models.CharField(max_length=30)
+    menu_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    user = models.ForeignKey(UserProfile,on_delete=models.CASCADE, related_name='review')
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE, related_name='review')
+    content = models.TextField(blank=True)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.content
+
+
