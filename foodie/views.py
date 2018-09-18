19a9558603a8c5e5f9671f017075e2cd93ad378f
wongsitu <sa_waika_4d_23@hotmail.com>
2018-09-18 16:22:08 -0700

    Set routes for html

--- foodie/views.py
+++ foodie/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+def index(request):
+    return render(request, 'reddit/index.html')
+
+def about(request):
+    return  render(request, 'reddit/about.html')
