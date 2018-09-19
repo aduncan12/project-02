d6791589dbd6bdb84616226df9b6ad5394088125
okikokim808 <kikomanistheman15@gmail.com>
2018-09-18 20:55:41 -0700

    fixed merge conflicts

--- foodie_django/urls.py
+++ foodie_django/urls.py
@@ -5,11 +5,4 @@ from django.conf.urls import url, include
 urlpatterns=[
     path('admin/', admin.site.urls),
     path('', include('foodie.urls')),
-<<<<<<< HEAD
-    # path('special', views.special, name='special'),
-    # path('foodie/', include('foodie.urls')),
-    # path('logout', views.user_logout, name='logout'),
 ]
-=======
-]
->>>>>>> adbd4b62d5788f3ad55c9241f665c0a86efedc72
