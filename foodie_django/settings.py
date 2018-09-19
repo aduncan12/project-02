9b06e089bc53a9cfa41d7261fa9233da8a00d540
wongsitu <sa_waika_4d_23@hotmail.com>
2018-09-19 16:50:14 -0700

    Thjings

--- foodie_django/settings.py
+++ foodie_django/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foodie'
+    'foodie',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
