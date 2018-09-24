0b3eb1d1ed02267549e1444a48aa81bd87cf2169
okikokim808 <kikomanistheman15@gmail.com>
2018-09-24 14:21:12 -0700

    added requirements

--- foodie_django/settings.py
+++ foodie_django/settings.py
@@ -127,7 +127,7 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/foodie/user_login/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
